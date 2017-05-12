eb7af40d5a528e72c58b210726de252cf9bad728
duhow <contacto@duhowpi.net>
2017-05-12 13:59:08 +0200

    Fix delete function values.

--- src/Sender.php
+++ src/Sender.php
@@ -280,12 +280,18 @@ class Sender {
 	function delete($message = NULL, $chat = NULL){
 		if($message === TRUE or (empty($message) && !isset($this->context['message_id']))){
 			$this->message(TRUE);
-		}elseif(is_array($message) and isset($message['message_id'])){
-			$this->message($message['message_id']);
+		}elseif(is_array($message) and isset($message["message_id"])){
+			$this->message($message["message_id"]);
+		}elseif(!empty($message)){
+			$this->message($message);
 		}
+
 		if($message === TRUE or (empty($chat) && !isset($this->context['chat_id']))){
 			$this->chat(TRUE);
+		}elseif(!empty($chat)){
+			$this->chat($chat);
 		}
+
 		$this->method = "deleteMessage";
 		return $this->send();
 	}
