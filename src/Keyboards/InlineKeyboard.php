4d88a16a1078b1c7bdce8b3413b70e582ac64ac4
duhow <contacto@duhowpi.net>
2017-05-26 11:32:18 +0200

    FIX Inline Keyboard start links.

--- src/Keyboards/InlineKeyboard.php
+++ src/Keyboards/InlineKeyboard.php
@@ -51,8 +51,9 @@ class InlineKeyboardRow {
 		$data['text'] = $text;
 		if(filter_var($request, FILTER_VALIDATE_URL) !== FALSE){ $data['url'] = $request; }
 		elseif($switch === TRUE or (is_string($switch) && strtolower($switch) == "command")){
-			// enviar por privado
-			$data['url'] = "https://telegram.me/" .$this->bot->username ."?start=" .urlencode($request);
+			// Iniciar por privado
+			$request = preg_replace("/([^-_a-zA-Z0-9]+)/i", "", $request); // Caracteres permitidos
+			$data['url'] = "https://t.me/" .$this->bot->username ."?start=" .$request;
 		}elseif(is_string($switch) && strtolower($switch) == "share"){
 			$enc = NULL;
 			if(is_array($request) && count($request) == 2){
@@ -60,13 +61,19 @@ class InlineKeyboardRow {
 			}else{
 				$enc = ['url' => urlencode($request)];
 			}
-			$data['url'] = "https://telegram.me/share/url?" .http_build_query($enc);
+			$data['url'] = "https://t.me/share/url?" .http_build_query($enc);
+		}elseif(strtolower($switch) == "text"){
+			$data['switch_inline_query'] = $switch;
+			$data['callback_data'] = "T:" .$request;
+		}elseif(strtolower($switch) == "pay"){
+			$data['switch_inline_query'] = $switch;
+			$data['pay'] = TRUE;
+		}elseif($switch === FALSE){
+			$data['switch_inline_query'] = $request;
+		}else{
+			$data['switch_inline_query'] = $switch;
+			$data['callback_data'] = $request;
 		}
-		elseif($switch === FALSE){ $data['switch_inline_query'] = $request; }
-		elseif(is_string($switch) && strtolower($switch) == "text"){ $data['callback_data'] = "T:" .$request; }
-		elseif(is_string($switch) && strtolower($switch) == "pay"){ $data['pay'] = TRUE; }
-		elseif($switch === NULL or is_string($switch)){ $data['callback_data'] = $request; }
-		if(is_string($switch)){ $data['switch_inline_query'] = $switch; }
 		$this->buttons[] = $data;
 		return $this;
 	}
